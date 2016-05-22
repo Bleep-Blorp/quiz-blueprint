87e718a6a43228791f8902df8c3d2774764faf62
Brian Anderson <banderson623@gmail.com>
2016-05-21 22:38:50 -0500

    initial

---
+++ data/autotune.js
@@ -0,0 +1,27 @@
+{
+  "title": "Text Quiz",
+  "slug": "quiz-text",
+  "theme": "isu",
+  "questions":[
+    {
+      "type": "text/multiple_choice",
+      "question": "When using IPM for crops, what is the first question that needs to be answered about what you see in a field?",
+      "answers": [
+        "Is it normal?",
+        "How many pests are there?",
+        "What is the action threshold?",
+        "What is the appropriate management tactic?"
+      ]
+    },
+    {
+      "type": "text/multiple_choice",
+      "question": "What is the economic injury level when using IPM?",
+      "answers": [
+        "The lowest population density of a pest that will cause economic damage",
+        "The current cost of herbicide, insecticide, or fungicide",
+        "The amount of damage caused by management tactics",
+        "The current price per bushel of grain minus planting and harvest costs"
+      ]
+    }
+  ]
+}
